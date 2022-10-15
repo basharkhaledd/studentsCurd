de1c31a28fed4b6a1e7fb9683a760ba5a69e93fd
basharqawasmi <khaledbashar308@gmail.com>
2022-10-16 00:14:22 +0300

    Curd In Java2ee JSP & servlet

---
+++ src/main/java/com/example/studentsCurd/StudentsCurdApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentsCurd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsCurdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsCurdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentsCurd/StudentsCurdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentsCurd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsCurdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
